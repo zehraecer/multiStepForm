928109681486c0ed23c460fc04e6d3f31fe5c63a
Zehra Ecer <zehraecer1996@hotmail.com>
2024-03-20 23:50:31 +0300

    html css kodları girildi

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const selectplan = document.querySelector(".selectplan")
+const rightside = document.querySelector(".right-side")
+
+selectplan.addEventListener("click",function(){
+    console.log("sdkngflgn");
+    rightside.innerHTML = ``
+    
+})

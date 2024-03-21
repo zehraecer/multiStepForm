32b5a3e6b84407cdacf22bc889b311e9f116eaa7
Zehra Ecer <zehraecer1996@hotmail.com>
2024-03-21 23:03:13 +0300

    js sayfasını karmaşıklıktan kurtarmak için farklı yöntem denendi

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,152 +3,152 @@ const rightside = document.querySelector(".right-side-first")
 const addons = document.querySelector(".add-ons")
 const summary = document.querySelector(".summary")
 
-selectplan.addEventListener("click",function(){
-    console.log("sdkngflgn");
-    rightside.innerHTML = `
-    <h4>Select Your Plan</h4>
-            <h6>You have the option of monthly or yearly billing.</h6>
+// selectplan.addEventListener("click",function(){
+//     console.log("sdkngflgn");
+//     rightside.innerHTML = `
+//     <h4>Select Your Plan</h4>
+//             <h6>You have the option of monthly or yearly billing.</h6>
 
 
-            <div class="box">
+//             <div class="box">
                
-                <div class="box-one">
-                    <img src="assets/img/icon-arcade.svg" alt="">
+//                 <div class="box-one">
+//                     <img src="assets/img/icon-arcade.svg" alt="">
 
-                    <div class="box-one-text">
-                        <h4>Arcade</h4>
-                        <p>$90/yr</p>
-                        <h6>2 months free</h6>
-                    </div>
+//                     <div class="box-one-text">
+//                         <h4>Arcade</h4>
+//                         <p>$90/yr</p>
+//                         <h6>2 months free</h6>
+//                     </div>
                     
-                </div>
+//                 </div>
 
-                <div class="box-one">
-                    <img src="assets/img/icon-advanced.svg" alt="">
+//                 <div class="box-one">
+//                     <img src="assets/img/icon-advanced.svg" alt="">
 
-                    <div class="box-one-text">
-                        <h4>Arcade</h4>
-                        <p>$90/yr</p>
-                        <h6>2 months free</h6>
-                    </div>
+//                     <div class="box-one-text">
+//                         <h4>Arcade</h4>
+//                         <p>$90/yr</p>
+//                         <h6>2 months free</h6>
+//                     </div>
                     
-                </div>
+//                 </div>
 
-                <div class="box-one">
-                    <img src="assets/img/icon-pro.svg" alt="">
+//                 <div class="box-one">
+//                     <img src="assets/img/icon-pro.svg" alt="">
 
-                    <div class="box-one-text">
-                        <h4>Arcade</h4>
-                        <p>$90/yr</p>
-                        <h6>2 months free</h6>
-                    </div>
+//                     <div class="box-one-text">
+//                         <h4>Arcade</h4>
+//                         <p>$90/yr</p>
+//                         <h6>2 months free</h6>
+//                     </div>
                     
-                </div>
+//                 </div>
                 
-            </div>
+//             </div>
              
 
-            <div class="monthly-yearly-input">
-                  <h3>Monthly</h3>
-                  <input type="range" class="slider"  id="myRange" min="1" max="2" value="1">
-                  <h4>Yearly</h4>
-            </div>
-    `
+//             <div class="monthly-yearly-input">
+//                   <h3>Monthly</h3>
+//                   <input type="range" class="slider"  id="myRange" min="1" max="2" value="1">
+//                   <h4>Yearly</h4>
+//             </div>
+//     `
     
-})
+// })
 
 
-addons.addEventListener("click",function(){
-    rightside.innerHTML=`
-    <h4>Pick add-ons</h4>
-                    <h6>Add ons help enhance your gaming experience</h6>
+// addons.addEventListener("click",function(){
+//     rightside.innerHTML=`
+//                     <h4>Pick add-ons</h4>
+//                     <h6>Add ons help enhance your gaming experience</h6>
 
-                    <div class="right-side-hero">
+//                     <div class="right-side-hero">
                          
-                        <div class="right-side-hero-first">
+//                         <div class="right-side-hero-first">
                             
-                            <div class="hero-first-one">
-                                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+//                             <div class="hero-first-one">
+//                                 <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
 
-                                <div class="hero-first-text">
-                                    <h4>Online Service</h4>
-                                    <h6>Access to multiplayer games </h6>
-                                </div>
-                            </div>
-                            <h5>+$1/mo</h5>
-                        </div>
+//                                 <div class="hero-first-text">
+//                                     <h4>Online Service</h4>
+//                                     <h6>Access to multiplayer games </h6>
+//                                 </div>
+//                             </div>
+//                             <h5>+$1/mo</h5>
+//                         </div>
 
-                        <div class="right-side-hero-first">
+//                         <div class="right-side-hero-first">
 
-                            <div class="hero-first-one">
-                                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+//                             <div class="hero-first-one">
+//                                 <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
 
-                                <div class="hero-first-text">
-                                    <h4>Larger Storage</h4>
-                                    <h6>Extra 1TB of cloud save </h6>
-                                </div>
-                            </div>
-                            <h5>+$2/mo</h5>
-                        </div>
+//                                 <div class="hero-first-text">
+//                                     <h4>Larger Storage</h4>
+//                                     <h6>Extra 1TB of cloud save </h6>
+//                                 </div>
+//                             </div>
+//                             <h5>+$2/mo</h5>
+//                         </div>
 
 
-                        <div class="right-side-hero-first">
+//                         <div class="right-side-hero-first">
 
-                             <div class="hero-first-one">
-                                 <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+//                              <div class="hero-first-one">
+//                                  <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
      
-                                 <div class="hero-first-text">
-                                     <h4>Customizable profile</h4>
-                                     <h6>Custom theme on your profile</h6>
-                                 </div>
+//                                  <div class="hero-first-text">
+//                                      <h4>Customizable profile</h4>
+//                                      <h6>Custom theme on your profile</h6>
+//                                  </div>
 
-                             </div>
-                             <h5>+$2/mo</h5>
-                        </div>
+//                              </div>
+//                              <h5>+$2/mo</h5>
+//                         </div>
 
-                    </div>
-    `
-})
-
-
-summary.addEventListener("click",function(){
-    rightside.innerHTML=`
-    <h4>Finishing up</h4>
-    <h6>Double-check everything looks Ok before confirming</h6>
+//                     </div>
+//     `
+// })
 
 
-    <div class="right-summary-one">
-
-            <div class="right-side-summary">
-
-                <h4>Arcade (Monthly) </h4>
-                <h3>$9/mo</h3>
-            </div>
-
-            <div class="right-side-summary">
-
-                <h6>Online Service</h6>
-                <h2>$1/mo</h2>
-            </div>
-
-            <div class="right-side-summary">
-
-                <h6>Larger Storage</h6>
-                <h2>$9/mo</h2>
-            </div>
-
-    </div>
+// summary.addEventListener("click",function(){
+//     rightside.innerHTML=`
+//     <h4>Finishing up</h4>
+//     <h6>Double-check everything looks Ok before confirming</h6>
 
 
-    <div class="total">
+//     <div class="right-summary-one">
 
-        <h6>Total (per month)</h6>
-        <h3>$12/mo</h3>
-    </div>
+//             <div class="right-side-summary">
 
-</div>
+//                 <h4>Arcade (Monthly) </h4>
+//                 <h3>$9/mo</h3>
+//             </div>
 
-    `
-})
+//             <div class="right-side-summary">
+
+//                 <h6>Online Service</h6>
+//                 <h2>$1/mo</h2>
+//             </div>
+
+//             <div class="right-side-summary">
+
+//                 <h6>Larger Storage</h6>
+//                 <h2>$9/mo</h2>
+//             </div>
+
+//     </div>
+
+
+//     <div class="total">
+
+//         <h6>Total (per month)</h6>
+//         <h3>$12/mo</h3>
+//     </div>
+
+// </div>
+
+//     `
+// })
 
 

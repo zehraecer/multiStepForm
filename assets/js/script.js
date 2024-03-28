4d4e8fb16b892ba573ae373ee73499c0e650afaa
Zehra Ecer <zehraecer1996@hotmail.com>
2024-03-28 18:58:36 +0300

    nextbtn butonu için farklı yöntem denendi.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,49 @@ const myRange = document.querySelector("#myRange")
 const yearlyPlans = document.querySelectorAll(".yearly-plan")
 const monthlyPlans = document.querySelectorAll(".monthly-plan")
 const boxoneclicks = document.querySelectorAll(".box-one")
+const nexbtnone = document.querySelector(".next-btn-one")
+const nexbtntwo = document.querySelector(".next-btn-two")
+const nexbtnthree = document.querySelector(".next-btn-three")
+const nexbtnfour = document.querySelector(".next-btn-four")
+
+nexbtntwo.addEventListener("click", function () {
+    if (rightsidesecond.style.display = "block") {
+        rightsidesecond.style.display = "none"
+        rightsidethird.style.display = "block"
+    }
+})
+
+nexbtnthree.addEventListener("click", function () {
+    if (rightsidethird.style.display = "block") {
+        rightsidethird.style.display = "none"
+        rightsidefourth.style.display = "block"
+    }
+})
 
 
-// nextbtn.addEventListener("click", function () {
-//     if (rightsidefirst.style.display = "block") {
-//         rightsidefirst.style.display = "none"
-//         rightsidesecond.style.display = "block"
-//     }
-// })
+// function nextBtnRender() {
+//     const nextbtn = document.querySelector(".next-btn1")
+//     nextbtn.addEventListener("click", function () {
+//         if (rightsidefirst.style.display === "block") {
+//             console.log("iki");
+//             rightsidefirst.style.display = "none";
+//             rightsidesecond.style.display = "block";
+//         } else if (rightsidesecond.style.display === "block") {
+//             console.log("üç");
+//             rightsidesecond.style.display = "none";
+//             rightsidethird.style.display = "block";
+//         } else if (rightsidethird.style.display === "block") {
+//             console.log("dört");
+//             rightsidethird.style.display = "none";
+//             rightsidefourth.style.display = "block";
+//         }
+//     })
+// }
+
+
+
+
+
 
 for (const boxoneclick of boxoneclicks) {
     boxoneclick.addEventListener("click", function () {
@@ -39,38 +74,41 @@ for (const boxoneclick of boxoneclicks) {
 }
 
 
-myRange.addEventListener("click", function () {
-    if (myRange.value == "2") {
-        for (const yearlyPlan of yearlyPlans) {
-            yearlyPlan.style.display = "block"
-        }
-        for (const monthlyPlan of monthlyPlans) {
-            monthlyPlan.style.display = "none"
+function selectPlans() {
+    myRange.addEventListener("click", function () {
+        if (myRange.value == "2") {
+            for (const yearlyPlan of yearlyPlans) {
+                yearlyPlan.style.display = "block"
+            }
+            for (const monthlyPlan of monthlyPlans) {
+                monthlyPlan.style.display = "none"
+
+            }
 
         }
+        else {
+            for (const yearlyPlan of yearlyPlans) {
+                yearlyPlan.style.display = "none";
+            }
+            for (const monthlyPlan of monthlyPlans) {
+                monthlyPlan.style.display = "block";
+            }
+        }
+    })
+}
 
-    }
-    else {
-        for (const yearlyPlan of yearlyPlans) {
-            yearlyPlan.style.display = "none";
-        }
-        for (const monthlyPlan of monthlyPlans) {
-            monthlyPlan.style.display = "block";
-        }
-    }
-})
+
+
 
 form.addEventListener("submit", function (e) {
     e.preventDefault();
-    const nextbtn = document.querySelector(".next-btn")
     if (nextBtn1()) {
         console.log("dsjgfd");
-        nextbtn.addEventListener("click", function () {
-            if (rightsidefirst.style.display = "block") {
-                rightsidefirst.style.display = "none"
-                rightsidesecond.style.display = "block"
-            }
-        })
+        if (rightsidefirst.style.display = "block") {
+            rightsidefirst.style.display = "none"
+            rightsidesecond.style.display = "block"
+        }
+
     }
     else {
         console.log("dfgfdo");
@@ -107,40 +145,52 @@ function nextBtn1() {
 
 
 
-selectplan.addEventListener("click", function () {
-    // console.log("kdjgfb");
-    rightsidesecond.style.display = "none"
-    rightsidefirst.style.display = "block"
-    rightsidethird.style.display = "none"
-    rightsidefourth.style.display = "none"
-})
 
-yourinfo.addEventListener("click", function () {
-    // console.log("jsng");
-    rightsidesecond.style.display = "block"
-    rightsidefirst.style.display = "none"
-    rightsidethird.style.display = "none"
-    rightsidefourth.style.display = "none"
+function changePage() {
+    selectplan.addEventListener("click", function () {
+        // console.log("kdjgfb");
+        rightsidesecond.style.display = "none"
+        rightsidefirst.style.display = "block"
+        rightsidethird.style.display = "none"
+        rightsidefourth.style.display = "none"
+    })
+
+    yourinfo.addEventListener("click", function () {
+        // console.log("jsng");
+        rightsidesecond.style.display = "block"
+        rightsidefirst.style.display = "none"
+        rightsidethird.style.display = "none"
+        rightsidefourth.style.display = "none"
+        selectPlans()
+
+    })
+
+    addons.addEventListener("click", function () {
+        // console.log("rjgn");
+        rightsidethird.style.display = "block"
+        rightsidesecond.style.display = "none"
+        rightsidefirst.style.display = "none"
+        rightsidefourth.style.display = "none"
+
+    })
+
+    summary.addEventListener("click", function () {
+        // console.log("sog");
+        rightsidethird.style.display = "none"
+        rightsidesecond.style.display = "none"
+        rightsidefirst.style.display = "none"
+        rightsidefourth.style.display = "block"
+    })
 
 
-})
+}
 
-addons.addEventListener("click", function () {
-    // console.log("rjgn");
-    rightsidethird.style.display = "block"
-    rightsidesecond.style.display = "none"
-    rightsidefirst.style.display = "none"
-    rightsidefourth.style.display = "none"
 
-})
 
-summary.addEventListener("click", function () {
-    // console.log("sog");
-    rightsidethird.style.display = "none"
-    rightsidesecond.style.display = "none"
-    rightsidefirst.style.display = "none"
-    rightsidefourth.style.display = "block"
-})
+changePage()
+// nextBtnRender()
+
+
 
 
 
